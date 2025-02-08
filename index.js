6469d1dec16bc33caa20eda9cef523a3507ce17a
kulwinder kaur <kkaur85815@gmail.com>
2025-02-07 21:21:45 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// pages/index.js
+import Navbar from "../components/Navbar";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to My web pages</h1>
+      <p>Explore the different features of React and Next.js</p>
+    </div>
+  );
+}
